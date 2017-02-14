01f17ebde72bd84868f13b12aeff042f17d86801
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-14 10:13:55 -0700

    still not there

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -12,7 +12,7 @@ class CommitsController < ApplicationController
   private
 
   def commit_params
-    decoded_params = JSON.parse(params.keys.first)['payload']['head_commit']
+    decoded_params = JSON.parse(params.keys.first)['head_commit']
     # decoded_params.require(:payload).permit(head_commit: [:id, :message, :timestamp, author: [:username]])[:head_commit]
   end
 
