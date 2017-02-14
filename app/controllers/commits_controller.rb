6734018164de79e646151e1cba30db312e3033a2
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-14 10:10:21 -0700

    It was lugnut

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -12,16 +12,16 @@ class CommitsController < ApplicationController
   private
 
   def commit_params
-    decoded_params = ActionController::Parameters.new(JSON.parse(params.keys.first))
-    decoded_params.require(:payload).permit(head_commit: [:id, :message, :timestamp, author: [:username]])[:head_commit]
+    decoded_params = JSON.parse(params.keys.first)['payload']['head_commit']
+    # decoded_params.require(:payload).permit(head_commit: [:id, :message, :timestamp, author: [:username]])[:head_commit]
   end
 
   def build_params
     {
-      sha: commit_params[:id][0..11],
-      message: commit_params[:message],
-      commit_timestamp: commit_params[:timestamp],
-      author: commit_params[:author][:username]
+      sha: commit_params['id'][0..11],
+      message: commit_params['message'],
+      commit_timestamp: commit_params['timestamp'],
+      author: commit_params['author']['username']
     }
   end
 
